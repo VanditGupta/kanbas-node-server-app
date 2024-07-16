7d2b7973d89e5c6852738190ee846d9391f0a34b
VanditGupta <vanditgupta22@gmail.com>
2024-07-16 02:57:09 -0400

    Implemented 4.2

--- Kanbas/Database/index.js
+++ Kanbas/Database/index.js
@@ -4,4 +4,4 @@ import assignments from "./assignments.js";
 import users from "./users.js";
 import grades from "./grades.js";
 import enrollments from "./enrollments.js";
-export { courses, modules, assignments, users, enrollments, grades };
+export default { courses, modules, assignments, users, enrollments, grades };
