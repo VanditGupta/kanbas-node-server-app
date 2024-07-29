08fa93220f43b3811a372dbce519d029eca504b9
VanditGupta <vanditgupta22@gmail.com>
2024-07-29 00:22:50 -0400

    Implemented 3.6.1

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentsRoutes from "./Kanbas/Assignments/routes.js";
 import cors from "cors";
+import UserRoutes from "./Users/routes.js";
 
 const CONNECTION_STRING =
   process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
@@ -14,6 +15,7 @@ mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentsRoutes(app);
