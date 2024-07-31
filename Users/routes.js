f797b59865cc3e4f451810a5b1a4b31eaf751431
VanditGupta <vanditgupta22@gmail.com>
2024-07-31 01:43:40 -0400

    Reimplemented 5.1

--- Users/routes.js
+++ Users/routes.js
@@ -36,7 +36,13 @@ export default function UserRoutes(app) {
     res.json(status);
   };
   const signup = async (req, res) => {};
-  const signin = async (req, res) => {};
+
+  const signin = async (req, res) => {
+    const { username, password } = req.body;
+    currentUser = await dao.findUserByCredentials(username, password);
+    res.json(currentUser);
+  };
+
   const signout = (req, res) => {};
   const profile = async (req, res) => {};
 
